ebc76b8dda76b31105cffc0e7b6e967359b65b75
jinverno <65239409+jinverno@users.noreply.github.com>
2020-07-15 08:46:22 +0100

    Script with imports

---
+++ your-project/Project3_JIRM.py
@@ -0,0 +1,31 @@
+#### Imports ####
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import matplotlib as plt
+
+
+
+
+#### Joao Inverno API test area #### 
+
+
+
+
+
+
+#### Rafael Mello API  test area #### 
+
+
+
+
+
+
+#### Graphs area #### 
+
+
+
+
+
+
